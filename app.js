e23e669c3a5a0bb231464672b8163ac155e23b36
agustinadimarco8 <agustinadm93@gmail.com>
2021-05-16 22:22:39 -0300

    modificando archivos

---
+++ app.js
@@ -0,0 +1,10 @@
+const express= require("express");
+const app= express();
+
+const path= require("path");
+
+app.listen(3003, () => console.log("Servidor corriendo"))
+
+app.use(express.static(path.resolve(__dirname,"./public")))
+
+app.get("/", (req,res) => res.sendFile(path.join(__dirname,"/views/index.html")))
