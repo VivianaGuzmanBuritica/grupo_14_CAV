c80f2bb6c9a152947e0f2536fb154d2339009f5a
unknown <vivianaguzmanburitica@gmail.com>
2021-05-26 16:34:46 -0500

    pagina de login, vinculos al app.js

--- app.js
+++ app.js
@@ -8,3 +8,5 @@ app.listen(3003, () => console.log("Servidor corriendo"))
 app.use(express.static(path.resolve(__dirname,"./public")))
 
 app.get("/", (req,res) => res.sendFile(path.join(__dirname,"./views/index.html")))
+app.get('/login', (req, res)=> res.sendFile(path.resolve(__dirname, './views', 'login.html')))
+app.get('/register', (req, res)=> res.sendFile(path.resolve(__dirname, './views', 'register.html')))
