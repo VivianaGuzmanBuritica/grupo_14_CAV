3e29a75516235de90934ab341cdbe7d428df060e
agustinadimarco8 <agustinadm93@gmail.com>
2021-05-26 16:27:29 -0300

    archivo index

--- app.js
+++ app.js
@@ -7,4 +7,4 @@ app.listen(3003, () => console.log("Servidor corriendo"))
 
 app.use(express.static(path.resolve(__dirname,"./public")))
 
-app.get("/", (req,res) => res.sendFile(path.join(__dirname,"/views/index.html")))
+app.get("/", (req,res) => res.sendFile(path.join(__dirname,"./views/index.html")))
