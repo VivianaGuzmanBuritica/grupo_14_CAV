489b59f0f1a39e4c83869baf69bb56cd14add4a9
Christian Novello <83038255+chnovello@users.noreply.github.com>
2021-05-26 16:17:37 -0300

    prueba coomit

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const path= require('path');
 const express=require('express');
 const app= express();
 
-app.listen(3001, ()=>console.log('Bien hecho'));
+app.listen(3001, ()=>console.log('Bien hecho, servidor activo puerto:3001'));
 app.get('/', function(req, res){res.send('Hola')});
