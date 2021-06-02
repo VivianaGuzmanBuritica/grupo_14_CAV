6736de31e3ffb2a6589e6c08608b5d10d34c81c0
DESKTOP-M2INT4L\Viviana Guzmán <vivianaguzmanburitica@gmail.com>
2021-06-02 17:05:25 -0500

    product cart

--- app.js
+++ app.js
@@ -10,4 +10,5 @@ app.use(express.static(path.resolve(__dirname,"./public")))
 app.get("/", (req,res) => res.sendFile(path.join(__dirname,"./views/index.html")))
 app.get('/login', (req, res)=> res.sendFile(path.resolve(__dirname, './views', 'login.html')))
 app.get('/register', (req, res)=> res.sendFile(path.resolve(__dirname, './views', 'register.html')))
-  
+app.get('/productDetail', (req, res)=> res.sendFile(path.resolve(__dirname, './views', 'productDetail.html')))
+app.get('/productCart', (req, res)=> res.sendFile(path.resolve(__dirname, './views', 'productCart.html')))
