b0d7038d4f62321bcf91d4de522acbf870a127c0
agustinadm8 <agustinadm93@gmail.com>
2021-06-05 21:03:48 -0300

    home

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ app.listen(3003, () => console.log("Servidor corriendo puerto 3003"))
 app.use(express.static(path.resolve(__dirname,"./public")))
 
 app.get("/", (req,res) => res.sendFile(path.join(__dirname,"./views/index.html")))
-app.get('/login', (req, res)=> res.sendFile(path.resolve(__dirname, './views', 'login.html')))
-app.get('/register', (req, res)=> res.sendFile(path.resolve(__dirname, './views', 'register.html')))
-app.get('/productCart', (req, res)=> res.sendFile(path.resolve(__dirname, './views', 'productCart.html')))
-app.get('/productDetail', (req, res)=> res.sendFile(path.resolve(__dirname, './views', 'productDetail.html')))
+app.get("/login.html", (req, res)=> res.sendFile(path.resolve(__dirname, "./views/login.html")))
+app.get("/register.html", (req, res)=> res.sendFile(path.resolve(__dirname, "./views/register.html")))
+app.get("/productCart.html", (req, res)=> res.sendFile(path.resolve(__dirname, "./views/productCart.html")))
+app.get("/productDetail.html", (req, res)=> res.sendFile(path.resolve(__dirname, "./views/productDetail.html")))
 
   
