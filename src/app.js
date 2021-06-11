a173c1991173ba4322d047e669fc621d2d6c4229
Christian Novello <83038255+chnovello@users.noreply.github.com>
2021-06-10 23:00:00 -0300

    cambio de modelo SRC

--- src/app.js
+++ src/app.js
@@ -2,13 +2,24 @@ const express= require("express");
 const app= express();
 const path= require("path");
 
+// server start
 app.set("port", process.env.PORT || 3003)
-
 app.listen(app.get("port"),() => console.log("Servidor corriendo puerto:"+ app.get("port")))
 
-app.use(express.static(path.resolve(__dirname,"./public")))
 
-app.get("/", (req,res) => res.sendFile(path.join(__dirname,"./views/index.html")))
+// public access
+app.use(express.static(path.resolve(__dirname,"../public")))
+
+
+//  view engine
+app.set('view engine','ejs');
+app.set('views', path.resolve(__dirname, './views'));
+
+// routes
+const main = require('./routes/main');
+app.use(main);
+
+//app.get("/", (req,res) => res.sendFile(path.join(__dirname,"./views/index.html")))
 app.get("/login.html", (req, res)=> res.sendFile(path.resolve(__dirname, "./views/login.html")))
 app.get("/register", (req, res)=> res.sendFile(path.resolve(__dirname, "./views/register.html")))
 app.get("/productCart.html", (req, res)=> res.sendFile(path.resolve(__dirname, "./views/productCart.html")))
---
+++ src/routes/main.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+const main = require('../controllers/main');
+
+router.get('/',main.index);
+
+
+module.exports = router;
