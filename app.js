a4f73c24ff369fea1825b95402a63e07c8190df9
DESKTOP-M2INT4L\Viviana Guzmán <vivianaguzmanburitica@gmail.com>
2021-06-10 20:41:12 -0500

    cambio extenciones de html a ejs en app.js e index.ejs

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ app.listen(app.get("port"),() => console.log("Servidor corriendo puerto:"+ app.g
 
 app.use(express.static(path.resolve(__dirname,"./public")))
 
-app.get("/", (req,res) => res.sendFile(path.join(__dirname,"./views/index.html")))
-app.get("/login.html", (req, res)=> res.sendFile(path.resolve(__dirname, "./views/login.html")))
-app.get("/register", (req, res)=> res.sendFile(path.resolve(__dirname, "./views/register.html")))
-app.get("/productCart.html", (req, res)=> res.sendFile(path.resolve(__dirname, "./views/productCart.html")))
-app.get("/productDetail.html", (req, res)=> res.sendFile(path.resolve(__dirname, "./views/productDetail.html")))
+app.get("/", (req,res) => res.sendFile(path.join(__dirname,"./views/index.ejs")))
+app.get("/login", (req, res)=> res.sendFile(path.resolve(__dirname, "./views/login.ejs")))
+app.get("/register", (req, res)=> res.sendFile(path.resolve(__dirname, "./views/register.ejs")))
+app.get("/productCart", (req, res)=> res.sendFile(path.resolve(__dirname, "./views/productCart.ejs")))
+app.get("/productDetail", (req, res)=> res.sendFile(path.resolve(__dirname, "./views/productDetail.ejs")))
 
 app.set("view engine","ejs");
 
