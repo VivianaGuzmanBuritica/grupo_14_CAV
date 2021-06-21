a3afcedc343a17c6212a114e5af72e92861804dc
DESKTOP-M2INT4L\Viviana Guzmán <vivianaguzmanburitica@gmail.com>
2021-06-21 18:10:25 -0500

    ruta editar

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -11,7 +11,21 @@ const productController = {
         console.log(req.body);
         
         res.redirect("index")
+    },
+
+    edit:(req, res) => res.render('editProduct'),
+    editProduct: function(req,res){
+        let producto= {
+            name: req.body.name,
+            description: req.body.description,
+            category: req.body.category,
+            price: req.body.price,
+        }
+        console.log(req.body);
+        
+        res.redirect("index")
     }
+
 };
 
 module.exports = productController;
--- src/routes/product.js
+++ src/routes/product.js
@@ -5,8 +5,10 @@ const product = require('../controllers/product');
 
 router.get('/productDetail', product.productDetail);
 router.get('/newProduct', product.newProduct);
+router.get('/editProduct', product.edit);
 
 router.post('/newProduct', productController.createProduct);
+router.put('/editProduct',product.editProduct);
 
 
 module.exports = router;
