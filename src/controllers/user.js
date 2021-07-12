b33fa3c0797f8bd62c8f756acf7f8644beb6fbab
agustinadm8 <agustinadm93@gmail.com>
2021-07-12 10:10:17 -0300

    express validator

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,6 +1,7 @@
 const user = require('../models/user');
 const path = require('path');
 const fs = require('fs');
+const  { validationResult} = require("express-validator");
 
 
 const userController = {
@@ -17,28 +18,32 @@ const userController = {
     },
 
     userRegister: function (req, res) {
-        const directory = path.resolve(__dirname,"../data","users.json");
-        let usuarios = user.all();
-         let register = {
-            id: req.body.id ,
-            name: req.body.name,
-            email: req.body.email,
-            domicilio: req.body.domicilio,
-            tipo: req.body.tipo,
-            interes: req.body.interes,
-            fotoPerfil: req.body.fotoPerfil,
-            fechaNacimiento: req.body.fechaNacimiento,
-            password: req.body.password,
-            confirmPassword: req.body.confirmPassword,
+        let errors = validationResult(req);
+        if(errors.isEmpty()){
+            const directory = path.resolve(__dirname,"../data","users.json");
+            let usuarios = user.all();
+            let register = {
+                id: req.body.id ,
+                name: req.body.name,
+                email: req.body.email,
+                domicilio: req.body.domicilio,
+                tipo: req.body.tipo,
+                interes: req.body.interes,
+                fotoPerfil: req.body.fotoPerfil,
+                fechaNacimiento: req.body.fechaNacimiento,
+                password: req.body.password,
+                confirmPassword: req.body.confirmPassword,
             
-        };
-        usuarios.push(register)
-        fs.writeFileSync(directory,JSON.stringify(usuarios,null,2));
-        console.log(usuarios);
+            };
+            usuarios.push(register)
+            fs.writeFileSync(directory,JSON.stringify(usuarios,null,2));
+            console.log(usuarios);
 
-        res.redirect('ingresar')
-        return true;   
-      
+            res.redirect('ingresar')
+            return true;   
+        } else {
+            res.render('users/register', {errors: errors.array()})
+        }
     },
  
     show: (req, res) => res.render("users/profile", { user: user.one(req.params.id) }), //mostrata el detalle del perfil del usuario//
--- src/routes/user.js
+++ src/routes/user.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const user = require('../controllers/user');
 const multer = require('multer');
 const path = require('path');
+const {body} = require("express-validator");
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -15,11 +16,23 @@ const storage = multer.diskStorage({
      
     const upload = multer({storage})
 
+// Validaciones
+
+const validaciones = [
+  body("name").notEmpty().withMessage("Campo obligatorio"),
+  body("email").notEmpty().withMessage("Campo obligatorio").bail()
+  .isEmail().withMessage("El email no es válido"),
+  body("domicilio").notEmpty().withMessage("Campo obligatorio"),
+  body("date").notEmpty().withMessage("Campo obligatorio"),
+  body("password").notEmpty().withMessage("Campo obligatorio").bail()
+  .isLength({min:8}).withMessage("La contraseña debe tener 8 caracteres como mínimo"),
+];
+
 router.get('/ingresar', user.login);
 router.post('/ingresar',[upload.any()],user.userLogin);
 
 router.get('/registro',user.register);
-router.post('/registro',[upload.any()],user.userRegister); 
+router.post('/registro',validaciones,[upload.any()],user.userRegister); 
 
 router.get('/perfil', user.profile);
 router.post('/perfil', user.profile);
