8487b058ca3a859d18c70ae246025b3437cb92ed
agustinadm8 <agustinadm93@gmail.com>
2021-07-20 12:23:14 -0300

    middleware

--- src/middlewares/validateRegister.js
+++ src/middlewares/validateRegister.js
@@ -10,8 +10,8 @@ const validaciones = [
     body("domicilio").notEmpty().withMessage("El domicilio es campo obligatorio"),
     body("tipo").notEmpty().withMessage("El tipo es campo obligatorio"),
     body("interes").notEmpty().withMessage("El interes es campo obligatorio"),
-    //body("fotoPerfil").notEmpty().withMessage("La foto es campo obligatorio"),
-    //body("date").notEmpty().withMessage("Nacimiento es campo obligatorio"),
+    body("fotoPerfil").notEmpty().withMessage("La foto es campo obligatorio"),
+    body("date").notEmpty().withMessage("Nacimiento es campo obligatorio"),
     body("password").notEmpty().withMessage("Contraseña es campo obligatorio").bail()
     .isLength({min:8}).withMessage("La contraseña debe tener 8 caracteres como mínimo")
   ];
