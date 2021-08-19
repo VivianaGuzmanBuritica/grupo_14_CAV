e2739e545abe090cfe7da9f68a285ba10e58e131
Viviana <vivianaguzmanburitica@gmail.com>
2021-08-19 10:26:54 -0500

    product list

--- src/database/models/Product.js
+++ src/database/models/Product.js
@@ -2,57 +2,58 @@ module.exports = function (Sequelize, dataTypes) {
 
     let alias = 'Product';
     let cols = {
-        id_producto: {
+        id_product: {
             type: dataTypes.INTEGER,
             primaryKey: true,
             autoIncrement: true
         },
-        nombre: {
+        name: {
             type: dataTypes.STRING,
         },
-        id_marca: {
+        id_brand: {
             type: dataTypes.INTEGER,
         },
-        descripcion: {
+        description: {
             type: dataTypes.STRING,
         },
-        imagen: {
+        image: {
+            type: dataTypes.STRING,
+        },
+        id_category: {
             type: dataTypes.INTEGER,
         },
-        id_categoria: {
-            type: dataTypes.INTEGER,
-        },
-        precio: {
+        price: {
             type: dataTypes.INTEGER,
         },
         createAt: {
             type: dataTypes.DATE,
-            allowNull: false,
+            allowNull: true,
             defaultValue: dataTypes.now
         },
         updatedAt: {
             type: dataTypes.DATE,
-            allowNull: false,
+            allowNull: true,
             defaultValue: dataTypes.now
         }
     }
 
     let config = {
         tableName: 'products',
-        timestamps: true
+        timestamps: false
     }
 
     let Product = Sequelize.define(alias, cols, config);
 
     Product.associate = function (models) {
         Product.belongsTo(models.Marca, {
-            as: 'marca',
-            foreignkey: 'id_marca'
+            as: 'marcas',
+            foreignKey: 'id_brand'
         });
         
         Product.belongsTo(models.Categoria, {
-                as: 'categoria',
-                foreignkey: 'id_categoria'
+                as: 'categorias',
+                foreignKey: 'id_category',
+                targetKey: 'id_category'
             })
         }
 
