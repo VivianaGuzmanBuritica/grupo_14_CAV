24138b65a48eb2208b906447f301be0e7d702eae
Viviana <vivianaguzmanburitica@gmail.com>
2021-08-25 19:38:18 -0500

    home product list

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,5 +1,24 @@
+const product = require('../models/product');
+const path = require('path');
+const fs = require('fs');
+const sequelize = require('sequelize');
+const { all } = require('../models/product');
+const db = require('../database/models');
+const { Op } = sequelize;
+
 const controller = {
-    index:(req,res) => res.render('index')
+    index:(req, res) => {
+        db.Product.findAll()
+            .then(function (product) {
+                res.render('index', { product: product })
+            })
+            .catch(function (e) { console.log(e) })
+    },
 };
 
 module.exports = controller;
+// const controller = {
+//     index:(req,res) => res.render('index')
+// };
+
+// module.exports = controller;
--- src/routes/main.js
+++ src/routes/main.js
@@ -2,6 +2,22 @@ const express = require('express');
 const router = express.Router();
 const main = require('../controllers/main');
 
+
+const multer = require('multer');
+const path = require('path');
+
+let dest = multer.diskStorage({
+  destination: function (req, file, cb) {
+      let extension = path.extname(file.originalname);
+      if(extension.indexOf("jpg") > 0){
+          cb(null, path.resolve(__dirname,"../../public/uploads","products"))
+      }
+  },
+  filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now()+ path.extname(file.originalname))
+  }
+})
+const upload = multer({storage:dest});
 router.get('/',main.index);
 
 
