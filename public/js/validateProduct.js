3d51ae2d6425bd212c1a5198d704bc760231848e
Viviana <vivianaguzmanburitica@gmail.com>
2021-09-05 17:10:51 -0500

    validaciones producto basicas

--- public/js/validateProduct.js
+++ public/js/validateProduct.js
@@ -1,23 +1,63 @@
+// const {body} = require("express-validator");
+// const path = require('path');
+
 window.addEventListener(('load'), function () {
     let formulario = document.querySelector("form");
     formulario.addEventListener('submit', function (e) {
 
         e.preventDefault();
         let errores = [];
-        let campoNombre = document.querySelector('#name');
-        console.log(campoNombre);
 
+        //validación Nombre
+
+        let campoNombre = document.querySelector('#name');
         if (campoNombre.value == '') {
             errores.push('el campo de nombre debe estar completo');
         }
-        // else if (campoNombre > 3) {
-        //     errores.push('el campo nombre contener al menos 3 caracteres');
+        else if (campoNombre.value.length < 2) {
+            errores.push('el campo nombre debe contener al menos 2 caracteres');
+        }
+
+       
+        //validación Descripción
+
+        let campoDescripcion = document.querySelector('#description');
+        if (campoDescripcion.value == '') {
+            errores.push('el campo de Descripción debe estar completo');
+        }
+        
+        //validación Imagen
+
+        let campoImagen = document.querySelector('#image');
+       // let file = req.file;
+        //let acceptedExtensions = ['.jpg', '.png', '.gif'];
+
+        if (campoImagen.value == '') {
+            errores.push('Debe adjuntar al menos una imagen');
+        }
+
+        // else {
+        //     let fileExtension = path.extname(file.originalname).toLocaleLowerCase();
+        
+        //     if(!acceptedExtensions.includes(fileExtension)){
+        //       errores.push(`Las extensiones permitidas son' ${acceptedExtensions.join(', ')}`);
+        //     }
         // }
+
+        //validación Precio
+
+        let campoPrecio = document.querySelector('#price');
+        if (campoPrecio.value == '') {
+            errores.push('el campo de precio debe estar completo');
+        }
+        
+        //Array errores y mostrar en navegador
+
         if (errores.length > 0) {
             e.preventDefault();
 
-            let ulErrores = document.querySelector('div .errores ul');
-            for (i = 0; errores.length; i++) {
+            let ulErrores = document.querySelector('div.errores ul');
+            for (let i = 0; i < errores.length; i++) {
                 ulErrores.innerHTML += '<li>' + errores[i] + '</li>'
             }
         }
