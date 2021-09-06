0f1f42dd9caefdc63a3fa4fefacb495bea53c9e6
Viviana <vivianaguzmanburitica@gmail.com>
2021-09-06 16:16:55 -0500

    validacion front bajo cada input

--- public/js/validateProduct.js
+++ public/js/validateProduct.js
@@ -4,44 +4,47 @@
 window.addEventListener(('load'), function () {
     let formulario = document.querySelector("form");
     formulario.addEventListener('submit', function (e) {
-
-        e.preventDefault();
-        let errores = [];
+   
+        let errores = {
+            nombre: [],
+            descripcion: [],
+            imagen: [],
+            precio: [],
+        }
 
         //validación Nombre
 
         let campoNombre = document.querySelector('#name');
         if (campoNombre.value == '') {
-            errores.push('El campo de nombre debe estar completo');
+            errores.nombre.push('El campo Nombre debe estar completo');
         }
         else if (campoNombre.value.length < 2) {
-            errores.push('El campo nombre debe contener al menos 2 caracteres');
+            errores.nombre.push('El campo Nombre debe contener al menos 2 caracteres');
         }
 
-       
         //validación Descripción
 
         let campoDescripcion = document.querySelector('#description');
         if (campoDescripcion.value == '') {
-            errores.push('El campo de Descripción debe estar completo');
+            errores.descripcion.push('El campo Descripción debe estar completo');
         }
-        // else if (campoDescripcion.value.length < 2) {
-        //     errores.push('El campo descripción debe contener al menos 2 caracteres');
-        // }
-        
+        else if (campoDescripcion.value.length < 2) {
+            errores.descripcion.push('El campo Descripción debe contener al menos 2 caracteres');
+        }
+
         //validación Imagen
 
         let campoImagen = document.querySelector('#image');
-       // let file = req.file;
+        // let file = req.file;
         //let acceptedExtensions = ['.jpg', '.png', '.gif'];
 
         if (campoImagen.value == '') {
-            errores.push('Debe adjuntar al menos una imagen');
+            errores.imagen.push('Debe adjuntar una sola imagen en formato .jpg, .png ó .gif');
         }
 
         // else {
         //     let fileExtension = path.extname(file.originalname).toLocaleLowerCase();
-        
+
         //     if(!acceptedExtensions.includes(fileExtension)){
         //       errores.push(`Las extensiones permitidas son' ${acceptedExtensions.join(', ')}`);
         //     }
@@ -51,17 +54,54 @@ window.addEventListener(('load'), function () {
 
         let campoPrecio = document.querySelector('#price');
         if (campoPrecio.value == '') {
-            errores.push('El campo de precio debe estar completo');
+            errores.precio.push('El campo Precio debe estar completo');
         }
-        
-        //Array errores y mostrar en navegador
+        else if (campoPrecio.value == Number) {
+            errores.precio.push('Digite solo numeros separando con punto los')
+        }
 
-        if (errores.length > 0) {
+        //Array errores nombre
+
+        if (errores.nombre.length > 0) {
             e.preventDefault();
 
-            let ulErrores = document.querySelector('div.errores ul');
-            for (let i = 0; i < errores.length; i++) {
-                ulErrores.innerHTML += '<li>' + errores[i] + '</li>'
+            let ulErrores = document.querySelector('div.erroresNombre ul');
+            for (let i = 0; i < errores.nombre.length; i++) {
+                ulErrores.innerHTML += '<li>' + errores.nombre + '</li>'
+            }
+        }
+
+
+        //Array errores descripcion
+
+        if (errores.descripcion.length > 0) {
+            e.preventDefault();
+
+            let ulErrores = document.querySelector('div.erroresDescripcion ul');
+            for (let i = 0; i < errores.descripcion.length; i++) {
+                ulErrores.innerHTML += '<li>' + errores.descripcion + '</li>'
+            }
+        }
+
+        //Array errores imagen
+
+        if (errores.imagen.length > 0) {
+            e.preventDefault();
+
+            let ulErrores = document.querySelector('div.erroresImagen ul');
+            for (let i = 0; i < errores.imagen.length; i++) {
+                ulErrores.innerHTML += '<li>' + errores.imagen + '</li>'
+            }
+        }
+
+        //Array errores precio
+
+        if (errores.precio.length > 0) {
+            e.preventDefault();
+
+            let ulErrores = document.querySelector('div.erroresPrecio ul');
+            for (let i = 0; i < errores.precio.length; i++) {
+                ulErrores.innerHTML += '<li>' + errores.precio + '</li>'
             }
         }
 
