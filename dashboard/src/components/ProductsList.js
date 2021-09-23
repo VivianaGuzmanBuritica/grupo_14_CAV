589868bab2856b29b1fe3074be99f5b5b61965c0
Viviana <vivianaguzmanburitica@gmail.com>
2021-09-22 23:59:57 -0500

    api lista productos

--- dashboard/src/components/ProductsList.js
+++ dashboard/src/components/ProductsList.js
@@ -1,17 +1,32 @@
 
 import React from "react";
+import { useState, useEffect } from "react";
 import {Route, Link} from 'react-router-dom';
 import Products from "./Products";
 
 
 
 function ProductsList() {
+let [products, setProduct] = useState ([])
+
+useEffect(()=>{
+  fetch('http://localhost:3003/api/products')
+ .then(response => response.json())
+ .then(data => setProduct(data.data));
+},[])
+
+
+useEffect(()=>{
+  console.log('Me modifique');
+},[products])
+
   return (
     <React.Fragment>
     <div className="caja">
       <main>
         <h2>Lista de Productos</h2>
-    
+    {products.length ===0 && <li>Cargando información...</li>}
+        {products.map( product => <li>{product.name}</li>)}
         </main>
     </div>
     </React.Fragment>
